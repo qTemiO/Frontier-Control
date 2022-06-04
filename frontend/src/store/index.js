5517474658aa51e3295009c8cb55b9fd6ff9cf82
qTemiO <zhitihin.artem1441@yandex.ru>
2022-06-04 19:49:45 +0300

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,14 +5,15 @@ const SET_SEARCH_QUERY = 'SET_SEARCH_QUERY';
 const SET_LOADING = 'SET_LOADING';
 const SET_RESULT_RES = 'SET_RESULT_RES';
 const RESET_SEARCH = 'RESET_SEARCH';
-
+const SET_PROBITY_RES = 'SET_PROBILITY_RES';
 
 export default createStore({
   state: {
     state: {
       searchQuery: '',
       loading: false,
-      tnved: 'null'
+      tnved: 'null',
+      probility: 'null'
     },
   },
   getters: {
@@ -22,7 +23,8 @@ export default createStore({
     [SET_SEARCH_QUERY]: (state, searchQuery) => state.searchQuery = searchQuery,
     [SET_LOADING]: (state, loading) => state.loading = loading,
     [SET_RESULT_RES]: (state, tnved) => state.tnved = tnved,
-    [RESET_SEARCH]: state => state.tnved = null
+    [RESET_SEARCH]: state => state.tnved = null,
+    [SET_PROBITY_RES]: (state, prop) => state.probility = prop
   },
   actions: {
     setSearchQuery({commit}, searchQuery) {
@@ -33,7 +35,9 @@ export default createStore({
       try {
         const {data} = await axios.get(`http://localhost:8000/filter/recommendUser/${state.searchQuery}/`);
         commit(SET_RESULT_RES, data);
-        console.log(data)
+        let props = await axios.get(`http://localhost:8000/classificator/classificatorUser/${state.searchQuery}/`);
+        commit(SET_PROBITY_RES, props);
+        console.log(props)
       } catch (e) {
         commit(RESET_SEARCH);
       }
