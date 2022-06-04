06e4d9a9ac2f01425c1a0767c607c15c8fc32e16
Your Carma <y0urcarma@yandex.ru>
2022-06-04 18:25:27 +0300

    Docs

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <img src="../assets/hermes.png" height="32">
-    <a class="navbar-brand" href="#/">ПРОЕКТ "ВЕЛЕС"</a>
+    <a class="navbar-brand" href="#/">ПРОЕКТ "ГЕРМЕС"</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
