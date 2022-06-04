c78dc8f1cbe1bbd9f17aeb84ece9783deed85b1c
qTemiO <zhitihin.artem1441@yandex.ru>
2022-06-04 19:32:54 +0300

    API finally

--- backend/main/apps.py
+++ backend/main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-from filter.apps import FilterUserConfig
+from filter.apps import FilterUserConfig, FilterConfig
 from classificator.apps import ClassificatorConfig
 
 from loguru import logger
@@ -13,12 +13,30 @@ class MainConfig(AppConfig):
         df_res = ClassificatorConfig.df_res
         logreg = ClassificatorConfig.logreg
 
-        logger.debug(df_res)
-
         clf_prediction = ClassificatorConfig.predict(query, df_res, logreg)
-        vectorize_prediction = FilterUserConfig.predict(query, FilterUserConfig.tfidf_mat, FilterUserConfig.df, FilterUserConfig.vectorizer)
+        vectorize_prediction = FilterUserConfig.predict(query, FilterUserConfig.tfidf_mat, FilterUserConfig.VECTOR_USER_FILTER_DATAFRAME, FilterUserConfig.vectorizer)
 
-        logger.debug(clf_prediction)
-        logger.debug(vectorize_prediction)
+        classes = []
+        autochecker = False
+        for index, row in clf_prediction.iterrows():
+            if not autochecker:
+                if row['predict'] > 0.99: 
+                    autochecker=True
+                if row['predict'] > 0.01:
+                    classes.append(row['class'])
+        
+        classes_clf = list(set(classes))
+        classes_vectors = []
+        
+        for index, row in vectorize_prediction.iterrows():
+            if row['TNVED'] not in classes_vectors:
+                classes_vectors.append(row['TNVED'])
+        
+        codes = []
+        for class_ in classes_vectors:
+            if class_ in classes_clf:
+                codes.append(class_)
+
+        return codes
 
     logger.success('Main is ready!')
--- backend/main/views.py
+++ backend/main/views.py
@@ -5,14 +5,30 @@ from loguru import logger
 from rest_framework.views import APIView
 
 from .apps import MainConfig
+from filter.apps import FilterConfig
 
 class ComplexView(APIView):
 
     def get(self, request, query):
         logger.success(query)
         if not query: return HttpResponse(status=404)
-        predict = MainConfig.predict(query)
+        codes = MainConfig.predict(query)
 
-        results = []
+        smezh_df = FilterConfig.VECTOR_FILTER_DATAFRAME.copy(deep=True)
 
-        return JsonResponse({'data': results})
+        total_recommends = []
+        for text in smezh_df['KOD_TNVED_SPR'].apply(lambda x: str(x)):
+            for code in codes:
+                if str(code) in text[:len(str(code))]:
+                    total_recommends.append(str(text))
+
+        total_data = []
+        for recommend in total_recommends:
+            if not smezh_df[smezh_df['KOD_TNVED_SPR'] == int(recommend)].empty:
+                description = smezh_df[smezh_df['KOD_TNVED_SPR'] == int(recommend)]['OPISANIE_SPR'].values[0]
+                total_data.append({
+                    'code': recommend,
+                    'description': description
+                })
+
+        return JsonResponse({'data': total_data})
